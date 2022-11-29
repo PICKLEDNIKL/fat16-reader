67cfa0b073876e796d4c2ba942089962f8db40f2
PICKLEDNIKL <99259287+PICKLEDNIKL@users.noreply.github.com>
2022-11-29 17:54:55 +0000

    task 3 and 4

--- fat16reader.c
+++ fat16reader.c
@@ -1,3 +1,10 @@
+//USE HEXED.IT to see what im supposed to be reading
+
+//https:codeandlife.com/2012/04/07/simple-fat-and-sd-tutorial-part-2/
+//http://www.maverick-os.dk/FileSystemFormats/FAT16_FileSystem.html
+
+
+
 //DEFINES
 #include <stdio.h>
 #include <sys/types.h>
@@ -6,9 +13,8 @@
 #include <unistd.h>
 #include <stdint.h> //uint8_t
 #include <string.h>
-#include <stdlib.h>
 #include <stddef.h>
-
+#include <stdlib.h>
 
 typedef struct __attribute__((__packed__)) 
 {
@@ -34,10 +40,38 @@ typedef struct __attribute__((__packed__))
 	uint8_t		BS_FilSysType[ 8 ];	// = 'FAT16   ' (Not 0 term.)
 } BootSector;
 
+typedef struct __attribute__((__packed__))
+{
+    uint8_t     DIR_Name[ 11 ];     // Non zero terminated string
+    uint8_t     DIR_Attr;           // File attributes
+    uint8_t     DIR_NTRes;          // Used by Windows NT, ignore
+    uint8_t     DIR_CrtTimeTenth;   // Tenths of sec. 0...199
+    uint16_t    DIR_CrtTime;        // Creation Time in 2s intervals
+    uint16_t    DIR_CrtDate;        // Date file created
+    uint16_t    DIR_LstAccDate;     // Date of last read or write
+    uint16_t    DIR_FstClusHI;      // Top 16 bits file's 1st cluster
+    uint16_t    DIR_WrtTime;        // Time of last write
+    uint16_t    DIR_WrtDate;        // Date of last write
+    uint16_t    DIR_FstClusLO;      // Lower 16 bits file's 1st cluster
+    uint32_t    DIR_FileSize;       // File size in bytes
+} Fat16State;
+
+typedef struct __attribute__((__packed__))
+{
+    unsigned long fatStart;
+    unsigned long dataStart;
+    unsigned char clusterSize;
+    unsigned short currentCluster;
+    unsigned long bytesUnreadCluster;
+    unsigned long bytesUnreadFile;
+} Fat16State;
+
+#define fatBufferSize 32
+
 int Fileopen()
 {
-    //int fd = open("fat16.img",O_RDONLY); //USE THIS WHEN TESTING ACTUAL WORK
-    int fd = open("skeleton.txt",O_RDONLY);
+    int fd = open("fat16.img",O_RDONLY); //USE THIS WHEN TESTING ACTUAL WORK
+    //int fd = open("skeleton.txt",O_RDONLY);
     if(fd < 0)
     {
         printf("FILE CANNOT OPEN\n");
@@ -50,9 +84,9 @@ int Fileopen()
     }      
 }
 
-int Fileoffset(int fd)
+int Fileoffset(int fd, int offset)
 {
-    int fOffset = lseek(fd, 0, SEEK_SET); //SECOND NUMBER IS THE AMOUNT OF SPACES I WANT TO MOVE OUT
+    int fOffset = lseek(fd, offset, SEEK_SET); //SECOND NUMBER IS THE AMOUNT OF SPACES I WANT TO MOVE OUT
     if (fOffset == -1)
     {
         printf("FILE CANNOT OFFSET\n");
@@ -67,22 +101,32 @@ int Fileoffset(int fd)
 
 int Fileread(int fd, void *ptr, size_t count)
 {
-    //memcpy(text, *BootSector, sizeof(struct BootSector));
-    //while (count != 30)
-    //{   
-        read(fd, ptr, count);
-        //printf("%s", text);
-
-        //count++;
-    //}
-    //read(fd, buffer, 20);
-    //printf("buffer = %c\n", *buffer);
+    read(fd, ptr, count);   //count-1????
+    return ptr;
 }
 
-// int Fprint()
-// {
-//     //printf("%s", *text);
-// }
+void BSprint(BootSector *ptr)
+{
+    for (int i = 0; i<sizeof(BootSector); i++)
+    {
+        printf("%s", ptr[i]);
+    }
+
+    printf("\n");
+    printf("BytsPerSec: %i\n", ptr ->BPB_BytsPerSec);
+    printf("SecPerClus: %i\n", ptr ->BPB_SecPerClus);
+    printf("RsvdSecCnt: %i\n", ptr ->BPB_RsvdSecCnt); //number of reserved sectors
+    printf("NumFATs: %i\n", ptr ->BPB_NumFATs);
+    printf("RootEntCnt: %i\n", ptr ->BPB_RootEntCnt);
+    printf("TotSec16: %i\n", ptr ->BPB_TotSec16);
+    printf("FATSz16: %i\n", ptr ->BPB_FATSz16);
+    printf("TotSec32: %i\n", ptr ->BPB_TotSec32);
+    
+    for (int i = 0; i<11; i++)
+    {
+        printf("VolLab: %c\n", ptr ->BS_VolLab[i]);
+    }
+}
 
 int Fileclose(int fd)
 {
@@ -97,17 +141,24 @@ int main()
     //OPEN
     int fd = Fileopen();
 
+    int offset = 0; //change value to change offset
     //OFFSET
-    int offset = Fileoffset(fd);
+    int foffset = Fileoffset(fd, offset);
 
     //READ
+    BootSector* bp = malloc(sizeof(BootSector));
+    size_t count = sizeof(BootSector);
+    Fileread(fd, bp, count);
 
-    char something[50];
-    char text;
-    size_t count = sizeof something;
-    void *ptr;
-    
-    Fread(fd, ptr, count);
+    //BOOTSECTOR PRINT
+    BSprint(bp);
+    size_t fatsize = bp->BPB_BytsPerSec * bp->BPB_FATSz16;
+    uint16_t fat[fatsize/2];
+    Fileread(fd, &fat, fatsize);
 
-    Fclose(fd);
+    while(fatsize > 0 && *fat != 0xFFFF)
+
+    //CLOSE
+    Fileclose(fd);
 }
+
